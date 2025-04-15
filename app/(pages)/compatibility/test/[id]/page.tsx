b9b6fa5b5385e4d6df6334cb831eaf09feedf48c
tarunnagpal7 <tarunnagpal192004@gmail.com>
2025-04-15 10:14:17 +0530

    Revert "adding some components"
    This reverts commit 2232d4b2c09988892bde8e4178af798a6b8d73ff.

--- app/(pages)/compatibility/test/[id]/page.tsx
+++ app/(pages)/compatibility/test/[id]/page.tsx
@@ -215,7 +215,9 @@ export default function CompatibilityTestPage() {
 
   if (isLoading) {
     return (
+      <div className="flex justify-center items-center min-h-screen">
        <Loading />
+      </div>
     );
   }
 
